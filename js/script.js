c64766407310a8932f0630a6a66ae3e33ef7b394
Welliton Da Costa <wellintoncosta.10@hotmail.com>
2022-02-12 23:54:00 -0300

    ksksklss

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const bar = document.getElementById('bar');
 const leave = document.getElementById('leave');
 const nav = document.getElementById('navbar');
+
 if (bar) {
  bar.addEventListener ('click', () => {
     nav.classList.add('active');
